5b784e71c86be6a648290bffdfea92cb8efdde65
jangacharysripadha <97660105+jangacharysripadha@users.noreply.github.com>
2024-05-01 10:05:54 +0530

    Update HelloWorld.java
    file updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("welcome to Jenkins..........."); 
-        System.out.println("Helloworld......"); 
+        System.out.println("Hi how are you......"); 
     }
 }
